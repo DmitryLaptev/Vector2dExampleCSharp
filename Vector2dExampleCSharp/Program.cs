41516fe3a4bc3e24e7f7b4caa6a1e21783a3003d
DmitryLaptev <60900680+DmitryLaptev@users.noreply.github.com>
2020-02-17 22:04:23 +0300

    Add files via upload

---
+++ Vector2dExampleCSharp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector2dExampleCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector2d a=new Vector2d(3, 5);
+            Console.WriteLine("Вектор a:");
+            Console.Write("Введите координату x:");
+            a.SetX();
+            Console.Write("Введите координату y:");
+            a.SetY();
+            Vector2d b=new Vector2d(4, 8);
+            Console.WriteLine("Вектор b:");
+            Console.Write("Введите координату x:");
+            b.SetX();
+            Console.Write("Введите координату y:");
+            b.SetY();
+            Vector2d c = a + b;
+            Console.Write("Сложение векторов:");
+            c.Print();
+            c = a - b;
+            Console.Write("Разность векторов:");
+            c.Print();
+            double scal = a / b;
+            Console.Write("Скалярное произведение векторов:");
+            Console.WriteLine(scal.ToString());
+            /*double chax=a.GetX();
+            double chay=a.GetY();
+            Console.WriteLine(chax.ToString());
+            Console.WriteLine(chay.ToString());*/
+            Console.WriteLine("Умножение вектора b на число");
+            Console.Write("Введите число:");
+            double ch = double.Parse(Console.ReadLine());
+            c = b * ch;
+            Console.Write("Результат умножения:");
+            c.Print();
+            double tangens=a.tanAngle(b);
+            Console.Write("Тангенс угла между векторами:");
+            Console.WriteLine(tangens.ToString());
+            /*chax = a.GetX();
+            chay = a.GetY();
+            Console.WriteLine(chax.ToString());
+            Console.WriteLine(chay.ToString());*/
+            c++;
+            Console.Write("Инкремент:");
+            c.Print();
+            c--;
+            Console.Write("Декремент:");
+            c.Print();
+            c += b;
+            Console.Write("Увеличение на координаты вектора b:");
+            c.Print();
+            c -= a;
+            Console.Write("Уменьшение на координаты вектора a:");
+            c.Print();
+            Console.WriteLine("Умножение вектора на число");
+            Console.Write("Введите число:");
+            ch = double.Parse(Console.ReadLine());
+            c *= ch;
+            Console.Write("Результат умножения:");
+            c.Print();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Vector2dExampleCSharp/Vector2d.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector2dExampleCSharp
+{
+    class Vector2d
+    {
+        double x;
+        double y;
+        public Vector2d()
+        {
+            x = 0;
+            y = 0;
+        }
+        public Vector2d(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Vector2d(double x1, double y1, double x2, double y2)
+        {
+            this.x = x2-x1;
+            this.y = y2-y1; 
+        }
+        public double GetX() => x;
+        public double GetY() => y;
+        public void SetX() => x = double.Parse(Console.ReadLine());
+        public void SetY() => y = double.Parse(Console.ReadLine());
+        Vector2d sum(Vector2d b)
+        {
+            x += b.x;
+            y += b.y;
+            return new Vector2d(x, y);
+        }
+        Vector2d sub(Vector2d b)
+        {
+            x -= b.x;
+            y -= b.y;
+            return new Vector2d(x, y);
+        }
+        double scalarMult(Vector2d b)
+        {
+            return x*b.x + y*b.y;
+        }
+        Vector2d mult(double value)
+        {
+            x *= value;
+            y *= value;
+            return new Vector2d(x, y);
+        }
+        public double length()
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+        }
+        public double tanAngle(Vector2d b)
+        {
+            return (x*b.y - y*b.x) / scalarMult(b);
+        }
+        public static Vector2d operator+(Vector2d v1,Vector2d v2)
+        {
+            return new Vector2d(v1.x + v2.x, v1.y + v2.y);
+        }
+        public static Vector2d operator-(Vector2d v1, Vector2d v2)
+        {
+            return new Vector2d(v1.x - v2.x, v1.y - v2.y);
+        }
+        public static double operator/(Vector2d v1, Vector2d v2)
+        {
+            return v1.x * v2.x + v1.y * v2.y;
+        }
+        public static Vector2d operator*(Vector2d vector, double realNumber)
+        {
+            return new Vector2d(realNumber * vector.x, realNumber * vector.y);
+        }
+        /*public static Vector2d operator ++(Vector2d v)
+        {
+            v.x++;
+            v.y++;
+            return new Vector2d(v.x,v.y); 
+        }*/
+        public static Vector2d operator ++(Vector2d v)
+        {
+            Vector2d temp= new Vector2d (v.x, v.y);
+            temp.x++;
+            temp.y++;
+            return temp;
+        }
+        public static Vector2d operator --(Vector2d v)
+        {
+            v.x--;
+            v.y--;
+            return new Vector2d(v.x, v.y);
+        }
+        ~Vector2d()
+        {
+            Console.WriteLine("Enabled"); 
+        }
+        /*public static Vector2d operator --(Vector2d v)
+        {
+            Vector2d temp= new Vector2d (v.x, v.y);
+            temp.x--;
+            temp.y--;
+            return temp;
+        }*/
+        public void Print()
+        {
+            Console.WriteLine("(" + x + ";" + y + ")");
+        }
+        public static explicit operator string(Vector2d v)
+        {
+            string s = "(" + v.x.ToString() + ";" + v.y.ToString() + ")";
+            return s;
+        }
+    }
+}
